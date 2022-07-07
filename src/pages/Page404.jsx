975d5968d8f3d24d08a75053c911824e14a1e5c6
Vladimir <vogisa@mail.ru>
2022-07-07 16:15:51 +0300

    +page 404 content

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import style from "./styles/Page404.module.scss";
 
 const Page404 = () => {
   document.title = "daze | Page Not Found";
 
-  return <>Page Not Found</>;
+  return (
+    <>
+      <div className="wrapper">
+        <div className={style.content}>
+          <h1>404</h1>
+          <p>Page Not Found</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Page404;
