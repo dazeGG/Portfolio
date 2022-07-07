5ac774d1bb41ca042b375300913e73b641c705f8
Vladimir <vogisa@mail.ru>
2022-07-07 16:31:18 +0300

    -react component

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -5,14 +5,12 @@ const Page404 = () => {
   document.title = "daze | Page Not Found";
 
   return (
-    <>
-      <div className="wrapper">
-        <div className={style.content}>
-          <h1>404</h1>
-          <p>Page Not Found</p>
-        </div>
+    <div className="wrapper">
+      <div className={style.content}>
+        <h1>404</h1>
+        <p>Page Not Found</p>
       </div>
-    </>
+    </div>
   );
 };
 
